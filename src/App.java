d27f60ee462c4ea24d098d44e0dcdf227aac1076
brunoapmiguel <20252394@iade.pt>
2025-12-06 00:18:30 +0000

    Codigo base

---
+++ src/App.java
@@ -0,0 +1,36 @@
+/*
+* Classes
+Quarto: id, numero, capacidade, estaOcupado
+Hospede: id, nome, documento
+Reserva: id, idQuarto, idHospede, numeroHospede, dataInicio, dataFim, ativa
+* */
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in); //Definição de "teclado" como input
+        //Declaração de variaveis
+        int numeroHospedes, opcMenu;
+        boolean estaOcupado;
+        //Definição de classes
+        class quarto {
+            int id, numero, capacidade;
+            boolean ocupado;
+        }
+        class hospede{
+            int id;
+            String nome, documento;
+        }
+        class reserva{
+            int id, idQuarto, idHospede, numeroHospede, dataInicio, dataFim;
+            boolean ativa;
+        }
+
+
+    }
+
+
+
+}
+
