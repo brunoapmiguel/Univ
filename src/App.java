b64e9a48827ad0463c735c3f42c6666761ab7e21
brunoapmiguel <20252394@iade.pt>
2025-12-06 01:04:55 +0000

    Codigo base

--- src/App.java
+++ src/App.java
@@ -27,7 +27,47 @@ public class App {
             boolean ativa;
         }
 
-
+        do {
+            System.out.println("== MENU ==");
+            System.out.println("1 - Quartos");
+            System.out.println("2 - Hospedes");
+            System.out.println("3 - Reservas");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opcao: ");
+            opcMenu = teclado.nextInt();
+            switch (opcMenu) {
+                case 1:
+                    System.out.println("== QUARTOS ==");
+                    System.out.println("1 - Listar todos os Quartos");
+                    System.out.println("2 - Listar Quartos livres");
+                    System.out.println("3 - Listar Quartos ocupados");
+                    System.out.println("0 - Sair");
+                    System.out.print("Escolha uma opcao: ");
+                    opcMenu = teclado.nextInt();
+                    break;
+                case 2:
+                    System.out.println("== HOSPEDES ==");
+                    System.out.println("1 - Listar Hospedes");
+                    System.out.println("2 - Procurar Hospede por documento");
+                    System.out.println("3 - Editar Hospede");
+                    System.out.println("0 - Sair");
+                    System.out.print("Escolha uma opcao: ");
+                    opcMenu = teclado.nextInt();
+                    break;
+                case 3:
+                    System.out.println("== RESERVAS ==");
+                    System.out.println("1 - Encontrar qualquer Quarto livre para determinada capacidade");
+                    System.out.println("2 - Selecionar Quarto especifico"); //Se indisponivel sugerir automaticamente uma alternativa adequada
+                    System.out.println("3 - Listar todas as Reservas");
+                    System.out.println("4 - Listar Reservas por Quarto"); //presentes ou futuras
+                    System.out.println("5 - Listar Reservas por Hospede"); //presentes ou futuras
+                    System.out.println("6 - Editar Reserva"); //numero de hospede ou datas e revalidar capacidade e conflitos
+                    System.out.println("0 - Sair");
+                    System.out.print("Escolha uma opcao: ");
+                    opcMenu = teclado.nextInt();
+                    break;
+            }
+        } while (opcMenu != 0);
     }
 
 
